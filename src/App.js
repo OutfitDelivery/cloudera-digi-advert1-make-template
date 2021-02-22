e418b26b84ae355ed7ad3ec70e18fd038dc9fd18
Matthew Magin <matthew.d.magin@gmail.com>
2021-02-22 10:17:41 +1000

    Update CSS + Add text conditional logic

--- src/App.js
+++ src/App.js
@@ -18,26 +18,32 @@ function App({
     const placeholderBkg = {
       backgroundImage: `url(https://cdn.jsdelivr.net/gh/OutfitDelivery/cloudera-digi-advert1-make-template@master/src/assets/Placeholder/Theme${theme}/${width/4}x${height/4}.png)`,
     };
-  
-  return (
-    <div className="App" data-theme={theme} data-headline-size={headlineSize} data-display-line-size={displayLineSize} style={appStyle}>
-    <div data-width={width/4} data-height={height/4}>
-      {/*<div className="placeholder" style={placeholderBkg}></div>*/}
-      <div className="text-area" data-theme={theme} data-text-type={contentType}>
-        <div className="subheadline text-el" data-max-line="2"><h2>{subheadline}</h2></div>
-        <div className="heading">
-          <div className="headline line-1 text-el" data-max-line="1"><h1>{headlineLine1}</h1></div>
-          <div className="headline line-2 text-el" data-max-line="1"><h1>{headlineLine2}</h1></div>
-          <div className="headline line-3 text-el" data-max-line="1"><h1>{headlineLine3}</h1></div>
-          <div className="headline line-4 text-el" data-max-line="1"><h1>{headlineLine4}</h1></div>
-          <div className="headline line-5 text-el" data-max-line="1"><h1>{headlineLine5}</h1></div>
+
+    function conditionalTextCheck(text, classList){
+      return text.props.children !== "" ? (<div className={classList} data-max-line="1">{text}</div>) : "";
+    }
+
+    var htmlStructure = (
+      <div className="App" data-theme={theme} data-headline-size={headlineSize} data-display-line-size={displayLineSize} style={appStyle}>
+        <div data-width={width/4} data-height={height/4}>
+          <div className="placeholder" style={placeholderBkg}></div>
+          <div className="text-area" data-theme={theme} data-text-type={contentType}>
+            <div className="subheadline text-el" data-max-line="2"><h2>{subheadline}</h2></div>
+            <div className="heading">
+              {conditionalTextCheck(<h1>{headlineLine1}</h1>, "headline line-1 text-el")}
+              {conditionalTextCheck(<h1>{headlineLine2}</h1>, "headline line-2 text-el")}
+              {conditionalTextCheck(<h1>{headlineLine3}</h1>, "headline line-3 text-el")}
+              {conditionalTextCheck(<h1>{headlineLine4}</h1>, "headline line-4 text-el")}
+              {conditionalTextCheck(<h1>{headlineLine5}</h1>, "headline line-5 text-el")}
+            </div>
+            {conditionalTextCheck(<h3>{displayLine}</h3>, "display-line text-el")}
+            {conditionalTextCheck(<p data-max-line="1">{callToAction}</p>, "call-to-action text-el")}
+          </div>
         </div>
-        <div className="display-line text-el" data-max-line="1"><h3>{displayLine}</h3></div>
-        <div className="call-to-action text-el"><p data-max-line="1">{callToAction}</p></div>
       </div>
-    </div>
-    </div>
-  );
+    );
+  
+  return htmlStructure;
 }
 
 App.propTypes = {
@@ -58,20 +64,20 @@ App.propTypes = {
 };
 
 App.defaultProps = {
-  width: 640,
-  height: 2400, 
-  theme: 1,
+  width: 4320,
+  height: 4320, 
+  theme: 2,
   contentType: "heading-3", 
   subheadline: "analytics", 
-  headlineLine1: "Three ways to achieve", 
-  headlineLine2: "machine learning", 
-  headlineLine3: "excellence", 
+  headlineLine1: "How to drive machine", 
+  headlineLine2: "learnings limitless", 
+  headlineLine3: "", 
   headlineLine4: "", 
   headlineLine5: "",
   headlineSize: 1,
-  displayLine: "potential", 
+  displayLine: "", 
   displayLineSize: 1,
-  callToAction: "read the white paper",
+  callToAction: "download",
 };
 
 
